68bda8456143fc72b0153864fbd7dbceee3a9459
Dmitry <dkv5674@mail.ru>
2024-02-21 18:55:34 +0300

    test

---
+++ js/app.js
@@ -0,0 +1,56 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1600,
+    delay: 200
+});
+
+ScrollReveal().reveal('.thumbnail, .arrows', { origin: 'bottom' });
